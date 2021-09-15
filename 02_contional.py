99bc88a0cb34979ed0810e11c411f932b55ad54b
Matthew <matthewlee031205@gmail.com>
2021-09-15 13:18:30 -0400

    Code added

--- 02_contional.py
+++ 02_contional.py
@@ -7,3 +7,6 @@ for i in range(5):
     principle += principle * interest_rate
 print(f'Principle: ${principle:0.2f}')
 # principle *= 1 + interest_rate  #equivalent but less readable
+
+cost = 0.75
+print(f'it costs ${cost:10.2f}')
